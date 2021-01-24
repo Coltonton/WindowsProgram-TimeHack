a4b952c4d53191bb7627eae5267bb20258745f0e
Coltonton <41667601+Coltonton@users.noreply.github.com>
2021-01-24 15:55:38 -0500

    Update Run_Example.cmd

--- Commands/Run_Example.cmd
+++ Commands/Run_Example.cmd
@@ -1,6 +1,7 @@
 set MMDDYYYY=%DATE:~4,2%-%DATE:~7,2%-%DATE:~10,4%
-date 01-18-2021
+date MM-DD-YYYY         ##
+C:
 cd \path\to\program\file
 start PROGRAMNAMEHERE.EXE
-timeout 5
+timeout 3
 date %MMDDYYYY%
